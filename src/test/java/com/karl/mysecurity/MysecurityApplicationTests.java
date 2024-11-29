e27ed5d571f4356614340dd3cb89fcc704199150
karl 朱 <karlzhu@karldeMac-mini.local>
2024-11-29 13:10:33 +0800

    init

---
+++ src/test/java/com/karl/mysecurity/MysecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karl.mysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
